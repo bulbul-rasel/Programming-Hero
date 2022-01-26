df8ec6a079b26c7546082982a769cedd09c6f95f
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-01-27 01:02:52 +0600

    push-pop

---
+++ Projects/Module 18/push-pop.js
@@ -0,0 +1,9 @@
+var friendList = ['kalam', 'emul', 'shuvo', 'akash'];
+friendList.push('noyan');
+friendList.push('kawsar');
+console.log(friendList);
+
+var popName = friendList.pop()
+// friendList.pop();
+console.log(friendList);
+console.log(popName);
