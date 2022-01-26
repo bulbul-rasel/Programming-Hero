9bd9df97865e91179d5371799ea1538354503c51
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-01-27 00:45:50 +0600

    array Inex

---
+++ Projects/Module 18/array-index.js
@@ -0,0 +1,8 @@
+var numbers = [32, 64, 24, 867, 45, 97, 97, 23, 49, 83];
+var index = numbers.indexOf(457);
+// var index = numbers.indexOf(45);
+var indexNmbr = numbers[2];
+numbers[3] = 87;
+console.log(numbers);
+console.log(indexNmbr);
+// console.log(index);
