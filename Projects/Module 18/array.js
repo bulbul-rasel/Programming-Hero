3803e99fa3481f47634150cc66148c02cb2fd912
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-01-27 00:24:48 +0600

    arry init

---
+++ Projects/Module 18/array.js
@@ -0,0 +1,10 @@
+var friendAge = [16, 22, 19, 21];
+var picnicTk = [105, 225, 25, 75];
+var naika = ['sabnur', 'opu', 'sabana', 'mahi'];
+
+// console.log(naika);
+// console.log(naika.length);
+console.log(picnicTk);
+var picnicTkCount = picnicTk.length;
+console.log(picnicTkCount);
+// console.log(friendAge);
