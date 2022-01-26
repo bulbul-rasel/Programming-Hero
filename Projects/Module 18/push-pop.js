10a5c9124869f278ef7d7504f39638a7a40ca2e4
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-01-27 01:07:27 +0600

    push-pop update for first element

--- Projects/Module 18/push-pop.js
+++ Projects/Module 18/push-pop.js
@@ -1,9 +1,12 @@
 var friendList = ['kalam', 'emul', 'shuvo', 'akash'];
 friendList.push('noyan');
 friendList.push('kawsar');
+
+var pushFirst = friendList.unshift('Emran');
 console.log(friendList);
 
 var popName = friendList.pop()
 // friendList.pop();
+var firstItem = friendList.shift();
 console.log(friendList);
 console.log(popName);
