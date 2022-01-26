546e9f4fc8060a6b5c22570054c24286ac0c409f
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-01-26 16:12:18 +0600

    conversion

---
+++ Projects/Module 17/conversion.js
@@ -0,0 +1,20 @@
+var firstName = 'Bulbul';
+var lastName = 'Rasel';
+// var fullName = (firstName + lastName);
+var fullName = (firstName + ' ' + lastName);
+
+// console.log(fullName);
+
+var eggPrice = '42.55';
+// var eggPriceNmbr = '42.55';
+var onionPrice = '38.33';
+// var onionPriceNmbr = 38.33;
+
+// var price = (eggPrice + onionPrice);
+// console.log(price);
+var eggPriceNmbr = parseInt(eggPrice);
+var onionPriceNmbr = parseInt(onionPrice);
+
+var priceNmbr = (eggPriceNmbr + onionPriceNmbr);
+console.log(priceNmbr);
+console.log(eggPriceNmbr);
---
+++ Projects/Module 17/math-operation.js
@@ -0,0 +1,26 @@
+var price1 = 44;
+var price2 = 32;
+
+var sum = price1 + price2;
+var diff = price1 - price2;
+var mul = price2 * price1;
+var div = price1 / price2;
+// console.log(sum);
+// console.log(diff);
+// console.log(mul);
+// console.log(div);
+
+// var newPrice = price2 + 10;
+price2 += 11;
+
+// console.log(price2);
+
+var age = 14;
+age++;
+console.log(age);
+
+var price = 100;
+price = price - 1;
+price -= 1;
+price--;
+console.log(price);
