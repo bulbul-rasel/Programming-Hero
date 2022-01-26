a56c481032ff539c15cb22dc4cc3998c58128116
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-01-26 17:22:11 +0600

    variable-types

---
+++ Projects/Module 17/variable-types.js
@@ -0,0 +1,14 @@
+var firstNmbr = 0.2;
+var secondNmbr = 0.3;
+
+var total = (firstNmbr + secondNmbr);
+var isRain = 'yes';
+var isSining = true;
+
+var toDay = new Date();
+console.log(total);
+console.log(toDay);
+console.log(typeof (total));
+console.log(typeof (isRain));
+console.log(typeof (isSining));
+console.log(typeof (toDay));
