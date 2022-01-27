942c52a2046bb774d8d0067889a2e391d2d0a3a5
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-01-27 13:42:35 +0600

    nested-condition

---
+++ Projects/Module 18/nested-condition.js
@@ -0,0 +1,22 @@
+var danishPrice = 50;
+var butterBan = 30;
+var tostPrice = 10;
+var packedWell = false;
+var myBudget = 88;
+
+// if (myBudget >= danishPrice) {
+//     console.log(' Ami Danish khey venish jamu...');
+// }
+// else if (myBudget >= butterBan) {
+//     console.log('Ami Ban kheabo chaye dubiye');
+// } else if (myBudget >= tostPrice) {
+//     console.log('Ami Toast diye cha khamu');
+// } else {
+//     console.log('Ami khamui naa.');
+// }
+
+if (myBudget >= danishPrice) {
+    if (packedWell == true) {
+        console.log("Ami Danish khamu Thik Ache")
+    } else { console.log('Nah mache dhuke ache') }
+}
