3f036f7eedac18de1b54f9b1672407c30688ed17
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-01-27 12:30:35 +0600

    conditional if else

---
+++ Projects/Module 18/conditional.js
@@ -0,0 +1,27 @@
+var isFoodReady = true;
+
+if (isFoodReady == true) {
+    console.log('Amma ami vat khabo...')
+} else {
+    console.log('Tahole pore khabo...')
+}
+
+var iphnPrice = 70000;
+var myBudget = 10000;
+
+if (myBudget >= iphnPrice) {
+    console.log('Ami iphone diye futani marbo')
+}
+if (myBudget <= iphnPrice) {
+    console.log('Amar Huawei phone e valo, futani mari na ami')
+}
+
+var gorurGosto = 200;
+var amarTk = 150;
+
+if (amarTk >= gorurGosto) {
+    console.log(' amr Gorur gosto khabo..!');
+}
+else {
+    console.log('Amar murgie valo lage gorute elergy ache')
+}
