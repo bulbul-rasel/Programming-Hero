900d6b2c6655d08983e0e6022a3916ecb514efb4
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-01-28 01:28:16 +0600

    loop recap

---
+++ Projects/Module 18/Module18.5/for-loop.js
@@ -0,0 +1,34 @@
+// For Loop 
+
+// for (var roastGiven = 0; roastGiven < 6; roastGiven++) {
+//     console.log('Ami Roast pai nai...! Please Roast den..');
+//     console.log(roastGiven);
+// }
+
+// for (var i = 0; i < 20; i++) {
+//     console.log(i);
+// }
+// for Even Number 
+
+// for (var i = 0; i < 20; i += 2) {
+//     console.log(i);
+// }
+// for Odd Number 
+
+// for (var i = 1; i < 20; i += 2) {
+//     console.log(i);
+// }
+
+// var numbers = [45, 35, 85, 45, 25, 95, 49, 73];
+
+// for (var i = 0; i < numbers.length; i++) {
+//     var element = numbers[i];
+//     console.log(element);
+// }
+
+var names = ['jodu', 'modu', 'kodu', 'lodu', 'vodu', 'nodu'];
+
+for (var i = 0; i < names.length; i++) {
+    var element = names[i];
+    console.log(element);
+}
---
+++ Projects/Module 18/Module18.5/while-loop.js
@@ -0,0 +1,16 @@
+// var roastGiven = 0;
+
+// while (roastGiven < 7) {
+//     console.log("Roast den please , Ami Roast pai nai");
+//     roastGiven++;
+//     console.log(roastGiven);
+// }
+
+
+// var number = 0; // For Even Number
+var number = 1;  //For odd Number
+
+while (number <= 20) {
+    console.log(number);
+    number = number + 2;
+}
