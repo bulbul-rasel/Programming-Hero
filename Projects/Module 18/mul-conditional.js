3a6a32b3d115f41792c8e23a7bd5b4b8dfdd20ba
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-01-27 12:57:51 +0600

    Multiple condition

---
+++ Projects/Module 18/mul-conditional.js
@@ -0,0 +1,14 @@
+var gotJob = true;
+var slary = 45000;
+var gotFlat = false;
+
+// if (gotJob == true && slary >= 50000 && gotFlat == true) {
+//     console.log('Cholo Biye kore feli')
+// } else {
+//     console.log('Nah.. Kopale biye nai')
+// }
+if ((gotJob == true && slary >= 50000) || gotFlat == true) {
+    console.log('Cholo Biye kore feli')
+} else {
+    console.log('Nah.. Kopale biye nai')
+}
