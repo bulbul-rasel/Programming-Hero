f4172de20ebe37a1546d52225d18312ef175e387
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-01-30 12:42:36 +0600

    Reverse factorial

--- Projects/Module 20/factorial.js
+++ Projects/Module 20/factorial.js
@@ -8,14 +8,54 @@
 
 // Factorial for function 
 
-function getFactorial(number) {
-    var factorial = 1;
+// function getFactorial(number) {
+//     var factorial = 1;
 
-    for (var i = 1; i <= number; i++) {
+//     for (var i = 1; i <= number; i++) {
+//         factorial = factorial * i;
+//     }
+//     return factorial;
+
+// }
+// var amarFactorial = getFactorial(7);
+// console.log('Factorial Value is :', amarFactorial);
+
+// Using While loop 
+
+// function myFactorial(number) {
+//     var factorial = 1;
+//     var i = 1;
+//     while (i <= number) {
+//         factorial = factorial * i;
+//         i++;
+//     }
+//     return factorial;
+// }
+// var amarFactorial = myFactorial(6);
+// console.log('Amar Factorial er Number :', amarFactorial);
+
+// Factorial reverse way
+
+// function getFactorial(number) {
+//     let factorial = 1;
+//     var i = number;
+//     while (i >= 1) {
+
+//         factorial = factorial * i;
+//         i--;
+//     }
+//     return factorial;
+// }
+
+// Using for loop 
+
+function getFactorial(number) {
+    let factorial = 1;
+    for (var i = number; i >= 1; i--) {
         factorial = factorial * i;
     }
-    return factorial;
-
+    return factorial
 }
-var amarFactorial = getFactorial(7);
-console.log('Factorial Value is :', amarFactorial);
+
+let amarFactorial = getFactorial(6);
+console.log('Amar factorial', amarFactorial)
