77cbdb2bfc0ce555b86244bf5a84c1cd8a7ad357
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-01-31 23:11:02 +0600

    Array suma

---
+++ Projects/Module 21/array-sum.js
@@ -0,0 +1,22 @@
+var arr = [43, 25, 82, 65, 73, 91, 58];
+var sum = 0;
+for (var i = 0; i < arr.length; i++) {
+    var element = arr[i];
+    sum = sum + element;
+
+}
+console.log(sum);
+
+// Using Function 
+
+function arraySum(number) {
+    var sum = 0;
+    for (let i = 0; i < number.length; i++) {
+        var element = number[i];
+
+        sum = sum + element;
+    }
+    return sum;
+}
+var total = arraySum([10, 20, 30]);
+console.log('Total Number is :', total)
