6fdcf1217f1fb7cbf6a21e36a7cc77a178f09271
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-02-06 23:48:25 +0600

    append child java script

---
+++ Projects/Module 24/first.js
@@ -0,0 +1,6 @@
+const secondTitle = document.getElementById('second-title');
+secondTitle.style.textAlign = 'center';
+secondTitle.style.color = ('red');
+secondTitle.style.backgroundColor = ('aqua');
+secondTitle.style.margin = '10px';
+secondTitle.style.padding = '20px';
