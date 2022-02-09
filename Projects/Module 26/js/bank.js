be3f604473a12e9e87d2648299b4c8c8fc9aac22
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-02-09 22:52:05 +0600

    login form

---
+++ Projects/Module 26/js/bank.js
@@ -0,0 +1,16 @@
+document.getElementById('login-submit').addEventListener('click', function () {
+    // for email 
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // for password 
+    const passwordFiled = document.getElementById('user-password');
+    const userPassword = passwordFiled.value;
+    // validation 
+    if (userEmail == 'sontan@baap.com' && userPassword == 'secret') {
+        window.location.href = 'banking.html';
+
+    } else {
+        alert('Please enter Valid Email and Password')
+    }
+
+})
