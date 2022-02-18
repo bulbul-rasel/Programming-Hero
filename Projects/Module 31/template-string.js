5925878ba98078cf91169cfc352fe03d80e79a29
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-02-19 01:43:29 +0600

    template string

---
+++ Projects/Module 31/template-string.js
@@ -0,0 +1,27 @@
+const priya = 'Opriya opriya tmi kothay '
+const kobita = "Kobita tmi sopno charini hoye khobor nio na"
+const bondhu = `meye tmi akhono amy bondhu vabo ki`
+
+// const tmi = 'tmi amr ghum tobu tomay 
+// niye sopno dekhi na
+// tmi amr khola hao na khono'
+
+const tmr = `tmi amr ghum tobu tomay 
+niye sopno dekhi na
+tmi amr khola hao na khono`
+
+const tmi = 'tmi amr ghum tobu tomay ' +
+    'niye sopno dekhi na' +
+    'tmi amr khola hao na khono'
+
+const count = 5;
+const friend = ['abul', 'babul', 'kabul', 'dabul']
+const old = '<h3 class = "start"> My serial is 5</h3>'
+const old2 = "<h3 class = 'start'> My serial is" + count + "</h3>"
+const new1 = `<h3 class = "start"> My serial is ${count}</h3>`
+const new2 = `<h3 class = "start"> My serial is ${friend.length}</h3>`
+
+const first = 'abul';
+const second = 'babul'
+const new3 = `His name is ${first}, his brother name is ${second}, is salary ${friend.length * 1000} taka`
+console.log(new3)
