b2e7283222e7ade8d0db3a3c4d16d13aa8089198
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-02-20 00:56:03 +0600

    advance

--- Projects/Module 32/map.js
+++ Projects/Module 32/map.js
@@ -1,4 +1,4 @@
-const numbers = [2, 4, 8, 12];
+// const numbers = [2, 4, 8, 12];
 
 // const dobuleIt = number => number * 2;
 
@@ -7,5 +7,15 @@ const numbers = [2, 4, 8, 12];
 // const output = numbers.map(number => number * 2);
 // console.log(output)
 
-const squers = numbers.map(x => x * x)
-console.log(squers)
+// const squers = numbers.map(x => x * x)
+// console.log(squers)
+
+const name = [
+
+    { 'name': 'jewel', 'age': '20' },
+    { 'name': 'bulbul', 'age': '22' },
+    { 'name': 'foyaz', 'age': '24' },
+    { 'name': 'mehedi', 'age': '28' },
+]
+const data = name.map(x => x['name'])
+console.log(data)
