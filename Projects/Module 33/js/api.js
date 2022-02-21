ba2f2f445faea078ac761deb25bceef4b63aa302
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-02-22 00:32:19 +0600

    load more api data

--- Projects/Module 33/js/api.js
+++ Projects/Module 33/js/api.js
@@ -3,3 +3,12 @@ function loadData() {
         .then(response => response.json())
         .then(json => console.log(json))
 }
+
+function loadUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => displayUsers(data))
+}
+function displayUsers(data) {
+    console.log(data)
+}
