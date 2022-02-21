07ec71fd411b232421e691a3145e2d0cb29ccef8
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-02-22 01:07:04 +0600

    load more api data dynamically

--- Projects/Module 33/js/api.js
+++ Projects/Module 33/js/api.js
@@ -10,5 +10,10 @@ function loadUsers() {
         .then(data => displayUsers(data))
 }
 function displayUsers(data) {
-    console.log(data)
+    const ul = document.getElementById('users')
+    for (const user of data) {
+        const li = document.createElement('li')
+        li.innerText = ` name: ${user.name}, email: ${user.email}`;
+        ul.appendChild(li)
+    }
 }
