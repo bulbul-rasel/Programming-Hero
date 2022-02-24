ca9ac7ba0d4e8a7c73aaaf1165a8a74fb1dc40d8
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-02-24 16:37:42 +0600

    practice upto 5 done

--- Projects/API practice/js/script.js
+++ Projects/API practice/js/script.js
@@ -11,14 +11,15 @@ const loadComment = () => {
 //     .then(data => displayComment(data))
 
 const displayComment = comments => {
-
+    console.log(comments);
     const commentContainer = document.getElementById('comments')
-    const first2comment = comments.slice(0, 2)
+    const first20comment = comments.slice(0, 20)
 
-    comments.forEach(comment => {
+    first20comment.forEach(comment => {
         const div = document.createElement('div')
         div.innerHTML = `
         <p>${comment.body} </p>
+        <button onClick="loadEmail('${comment.id}')">Find Email</button>
         `
         commentContainer.appendChild(div)
     })
@@ -30,4 +31,30 @@ const displayComment = comments => {
     //     `
     //     commentContainer.appendChild(div)
     // }
+}
+
+const loadEmail = (comment) => {
+    fetch(`https://jsonplaceholder.typicode.com/comments/${comment}`)
+        .then(response => response.json())
+        .then(data => displayEmail(data))
+}
+const displayEmail = emails => {
+    console.log(emails);
+    const emailContainer = document.getElementById('emails')
+    emailContainer.innerHTML = `
+    <h3>${emails.id} </h3>
+    <h5>${emails.name} </h5>
+    <p>${emails.email} </p>
+    `
+
+
+    // // const first20comment = comments.slice(0, 20)
+    // for (const email of emails) {
+    //     const div = document.createElement('div')
+    //     div.innerHTML = `
+    //         <p>${email.email} </p>
+    //         `
+    //     emailContainer.appendChild(div)
+    // }
+
 }
