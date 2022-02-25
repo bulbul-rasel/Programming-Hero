8cfcfc388e2b29214a657e671f119cb237917492
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-02-26 00:15:49 +0600

    call apply

---
+++ Projects/Module 36/call-apply.js
@@ -0,0 +1,33 @@
+const kebria = {
+    id: 101,
+    money: 10000,
+    name: 'Rj Kebria',
+    IsRich: true,
+    treatDay: function (expence, tips, tax) {
+        this.money = this.money - expence - tips - tax;
+        console.log(this);
+        return this.money
+    }
+}
+const heroBalam = {
+    id: 102,
+    money: 8000,
+    name: 'Rj Balam',
+    IsRich: false,
+}
+const heroKhan = {
+    id: 103,
+    money: 9000,
+    name: 'KK Khan',
+    IsRich: true,
+}
+kebria.treatDay(500, 100, 7)
+const heroTreat = kebria.treatDay.bind(heroBalam)
+heroTreat(200, 50, 5)
+const khanTreat = kebria.treatDay.bind(heroKhan)
+khanTreat(300, 50, 7)
+// Call 
+// kebria.treatDay.call(heroBalam, 500)
+
+//apply
+kebria.treatDay.apply(heroBalam, [1000, 100, 15])
