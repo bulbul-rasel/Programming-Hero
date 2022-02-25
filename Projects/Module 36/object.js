442944aaa4115f1e674659a50c9cac88ff8c91ae
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-02-25 20:42:54 +0600

    Object creatin

---
+++ Projects/Module 36/object.js
@@ -0,0 +1,25 @@
+const person = { name: "Shakib Al Hasan", job: "Cricketer" }
+
+const human = new Object()
+
+const student = Object.create(person)
+
+console.log(student.job);
+
+// Using Class 
+class People {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const man = new People('Manus', 12);
+console.log(man);
+
+// Using Functon 
+
+function Manus(name) {
+    this.name = name;
+}
+const peop = new Manus('Gaffar')
+console.log(peop);
