cae8cbb42cf027e34681a6c331bac75b7f3d66ba
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-03-08 02:01:07 +0600

    modue41

---
+++ Projects/Module 42/shortcut.js
@@ -0,0 +1,16 @@
+const money = 80;
+const myVar = 100;
+const food = money > 100 ? 'biriyani khabo' : 'vat khabo'
+const food1 = money > 70 && myVar > 80 ? 'Coke khabo' : 'pani khabo'
+// console.log(food1);
+
+// number to string 
+const num1 = 52; console.log(num1);
+const numStr = num1 + ''
+console.log(numStr);
+
+// string to number 
+
+const num2 = '522'
+const num = +num2
+console.log(num);
