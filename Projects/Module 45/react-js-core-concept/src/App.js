8eaa8291c0f37c7083547e724129cce1b16e9032
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-03-15 23:09:35 +0600

    component

--- Projects/Module 45/react-js-core-concept/src/App.js
+++ Projects/Module 45/react-js-core-concept/src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+const singers = [
+  { name: 'Dr Mahfuz', joib: 'singer' },
+  { name: 'Eva Rahman', joib: 'singer2' },
+  { name: 'Morich', joib: 'Agun e=r agun' },
+  { name: 'Hero Alam', joib: 'Tending' }
+]
+
 function App() {
+  const nayoks = ['BappaRaz', 'Shapik Khan', 'Kuber', 'Jashim', 'Jayed Khan']
   return (
     <div className="App">
-      <Person></Person>
+      {
+        nayoks.map(nayok => <li>Name :{nayok}</li>)
+      }
+      {/* {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      } */}
+      {
+        singers.map(singer => <Person name={singer.name}></Person>)
+      }
+      {/* <Person name={nayoks[0]} naika='Nai re Mama'></Person>
       <h5>New Component</h5>
       <Friend></Friend>
-      <Person></Person>
-      <Person></Person>
+      <h5>New Component</h5>
+      <Person name={nayoks[1]} naika='Apu Khan'></Person>
+      <Person></Person> */}
 
-    </div>
+    </div >
   );
 }
-function Person() {
+function Person(props) {
+  console.log(props);
   return (
     <div className='person'>
-      <h1>Shakib Al Hasan</h1>
-      <p>Profession: Cricketer</p>
+      <h1>{props.name}</h1>
+      <p>{props.naika} </p>
     </div>
   )
 }
