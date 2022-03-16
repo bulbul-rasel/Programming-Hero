5de5ad78e83b5bff540671e2c226a8cbbfcff307
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-03-17 00:25:34 +0600

    component

---
+++ Projects/Module 46/react-rest-countries/src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Person = () => {
+    return (
+        <div>
+            <h2>This is Person for sure..</h2>
+        </div>
+    );
+};
+
+export default Person;
