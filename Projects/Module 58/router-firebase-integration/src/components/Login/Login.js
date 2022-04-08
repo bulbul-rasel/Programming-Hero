0e87e99bf3018b08014b04a8cbb16c9a9acb6b9f
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-04-09 01:19:49 +0600

    addded auth login logout togglr

--- Projects/Module 58/router-firebase-integration/src/components/Login/Login.js
+++ Projects/Module 58/router-firebase-integration/src/components/Login/Login.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import useFirebase from '../hooks/useFirebase';
 import './Login.css'
 
 const Login = () => {
+
+    const { signInWithGoogle } = useFirebase()
     return (
         <div>
-            <h2>Login</h2>
+            <h1>Please Login..!!</h1>
+            <div className='googleSignIn'>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+            <form action="">
+                <input type="email" name="" id="" placeholder='Enter your name' />
+                <br />
+                <input type="password" name="" id="" placeholder='Enter your password' /><br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
---
+++ Projects/Module 58/router-firebase-integration/src/components/Register/Register.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <form action="">
+                <input type="text" placeholder='Your name' />
+                <br />
+                <input type="email" name="" id="" placeholder='Enter your name' />
+                <br />
+                <input type="password" name="" id="" placeholder='Enter your password' /><br />
+                <input type="submit" value="Register" />
+            </form>
+        </div>
+    );
+};
+
+export default Register;
